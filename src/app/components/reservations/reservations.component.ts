1d3737766b7914904e62200298b36f55dd7e254a
Marcin Szewczyk <marcinszewczyk.arch@gmail.com>
2022-01-06 15:57:04 +0100

    system properties directory

--- src/app/components/reservations/reservations.component.ts
+++ src/app/components/reservations/reservations.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, SimpleChanges} from '@angular/core';
 import {BookingClientService, Reservation} from "../../services/booking-client.service";
+import {formatDate} from "@angular/common";
 
 @Component({
   selector: 'app-reservations',
@@ -12,6 +13,13 @@ export class ReservationsComponent implements OnInit {
   allApartments: any;
   loadedReservationsAdd: any;
   loadedReservationsFilters: any;
+  todayDate: any;
+  daysBack: any;
+  daysForward: any;
+  dateBack: any;
+  dateForward: any;
+  futureDates: Array<string> = [];
+  pastDates: Array<string> = [];
 
   constructor(private bokingClientService: BookingClientService) {
   }
@@ -23,6 +31,10 @@ export class ReservationsComponent implements OnInit {
     this.bokingClientService.getApartments().subscribe(value => {
       this.allApartments = value;
     })
+
+    this.todayDate = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+
+    this.backAndForward(String(0), String(3));
   }
 
   loadReservationsAdd() {
@@ -43,6 +55,56 @@ export class ReservationsComponent implements OnInit {
     }
   }
 
+  backAndForward(back: string, forward: string) {
+    this.daysBack = back;
+    this.daysForward = forward;
+
+    //future date limit
+    var futureDate = new Date();
+    futureDate.setDate(futureDate.getDate() + Number(forward));
+    this.dateForward = formatDate(futureDate, 'yyyy-MM-dd', 'en');
+
+    //past date limit
+    var pastDate = new Date();
+    pastDate.setDate(pastDate.getDate() - Number(back));
+    this.dateBack = formatDate(pastDate, 'yyyy-MM-dd', 'en');
+
+    //future dates array
+    let x : number = 0;
+    let dateAdded = this.dateForward;
+    this.futureDates = [];
+
+    while(x < Number(forward)) {
+      x = x + 1;
+      var futureDate = new Date();
+      futureDate.setDate(futureDate.getDate() + x);
+      dateAdded = formatDate(futureDate, 'yyyy-MM-dd', 'en');
+      this.futureDates.push(dateAdded);
+    }
+
+    //past dates array
+    x = Number(back);
+    dateAdded = this.dateBack;
+    this.pastDates = [];
+
+    while(x > 0) {
+      var backDate = new Date();
+      backDate.setDate(backDate.getDate() - x);
+      dateAdded = formatDate(backDate, 'yyyy-MM-dd', 'en');
+      this.pastDates.push(dateAdded);
+      x = x - 1;
+    }
+
+  }
+
+  getWeekday(todayDate: string) : string {
+    let arrayOfWeekdays = ["niedziela", "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota"]
+    let date = new Date(todayDate);
+    let weekDayNumber = date.getDay();
+
+    return arrayOfWeekdays[weekDayNumber];
+  }
+
   delete() {
   }
 
@@ -50,6 +112,5 @@ export class ReservationsComponent implements OnInit {
   }
 
   action1() {
-
   }
 }
